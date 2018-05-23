76ccda3c26ee6579483f458bf3f99ea6d865c4d9
Uli Kusterer <ulivc@zathras.de>
2018-05-23 06:08:57 +0200

    Initial Commit

---
+++ GamePlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GamePlusPlus
+//
+//  Created by Uli Kusterer on 23.05.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
